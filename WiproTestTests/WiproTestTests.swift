0d575df29ee199c85150fddd8d63d8f7765da6b7
Randy Bannister <38363373+RandyBannister@users.noreply.github.com>
2022-07-11 15:48:49 -0500

    add test

--- WiproTestTests/WiproTestTests.swift
+++ WiproTestTests/WiproTestTests.swift
@@ -24,6 +24,24 @@ class WiproTestTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        let helper = InputConfirmHelper()
+        let emailResult1 = helper.checkEmail(email: "aa")
+        let emailResult2 = helper.checkEmail(email: "aa@aa.com")
+        XCTAssertTrue(emailResult1)
+        XCTAssertTrue(emailResult2)
+        
+        let passwordResult1 = helper.passwordCheck(pwd: "a")
+        let passwordResult2 = helper.passwordCheck(pwd: "aA")
+        let passwordResult3 = helper.passwordCheck(pwd: "aA!")
+        let passwordResult4 = helper.passwordCheck(pwd: "A@#22")
+        let passwordResult5 = helper.passwordCheck(pwd: "ASD@aa221#$")
+
+        XCTAssertEqual(passwordResult1, .upper)
+        XCTAssertEqual(passwordResult2, .special)
+        XCTAssertEqual(passwordResult3, .numbers)
+        XCTAssertEqual(passwordResult4, .sixCharacters)
+        XCTAssertEqual(passwordResult5, .right)
     }
 
     func testPerformanceExample() throws {
